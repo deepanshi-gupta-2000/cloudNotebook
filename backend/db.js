4396685e704a651685d477ece0316ce3732b8dc8
deepanshi-gupta-2000 <2000deepanshigupta@gmail.com>
2025-01-19 23:32:35 +0530

    changes done in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1/iNotebook?authSource=admin"
+const mongoURI = "mongodb://127.0.0.1:27017/iNotebook?authSource=admin"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI);
