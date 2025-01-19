2d6dfd39d85f6a0467e4e7c2021e0a3e3fa09c45
deepanshi-gupta-2000 <2000deepanshigupta@gmail.com>
2025-01-20 00:13:33 +0530

    db changes done

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook?authSource=admin"
+// const mongoURI = "mongodb://127.0.0.1:27017/iNotebook?authSource=admin"
+const mongoURI = "mongodb+srv://2000deepanshigupta:6Wj9QJyOm5ObvSaX@cluster0.sy3zb.mongodb.net/cluster0?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI);
